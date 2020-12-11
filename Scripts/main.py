9cf2de1b7f0d9262c54879fb24e415e86cb2fc27
ayushkumar25 <ayush2608@gmail.com>
2020-12-11 19:38:24 +0530

    Initial commit

---
+++ Scripts/main.py
@@ -0,0 +1,48 @@
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.uic import loadUi
+
+
+class Login(QDialog):
+    def __init__(self):
+        super(Login, self).__init__()
+        loadUi(r'C:\Users\KIIT\PycharmProjects\Login\Resource\login.ui', self)
+        self.loginbutton.clicked.connect(self.loginfunction)
+        self.createaccbutton.clicked.connect(self.gotocreate)
+
+    def loginfunction(self):
+        email = self.email.text()
+        password = self.password.text()
+        print("Successfully logged in with email: ", email, "and password:", password)
+
+    def gotocreate(self):
+        createacc = CreateAcc()
+        widget.addWidget(createacc)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+class CreateAcc(QDialog):
+    def __init__(self):
+        super(CreateAcc, self).__init__()
+        loadUi(r'C:\Users\KIIT\PycharmProjects\Login\Resource\createacc.ui', self)
+        self.signupbutton.clicked.connect(self.createaccfunction)
+
+    def createaccfunction(self):
+        email = self.email.text()
+        if self.password.text() == self.confirmpass.text():
+            password = self.password.text()
+            print("Successfully created acc with email: ", email, "and password: ", password)
+            login = Login()
+            widget.addWidget(login)
+            widget.setCurrentIndex(widget.currentIndex() + 1)
+
+
+
+app = QApplication(sys.argv)
+mainwindow = Login()
+widget = QtWidgets.QStackedWidget()
+widget.addWidget(mainwindow)
+widget.setFixedWidth(480)
+widget.setFixedHeight(620)
+widget.show()
+app.exec_()
