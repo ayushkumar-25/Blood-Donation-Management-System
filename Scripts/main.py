66ceeee3373b1fb0ca8684612be108d9e205e724
ayushkumar25 <ayush2608@gmail.com>
2020-12-12 08:48:09 +0530

    Authentication Completed

--- Scripts/main.py
+++ Scripts/main.py
@@ -2,7 +2,19 @@ import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication
 from PyQt5.uic import loadUi
+import pyrebase
 
+firebaseConfig = {'apiKey': "AIzaSyD-UKONeVgyG4wmi7Rym-lZYIn9CDpLI3Y",
+    'authDomain': "fir-auth-ddfd6.firebaseapp.com",
+    'databaseURL': "https://fir-auth-ddfd6-default-rtdb.firebaseio.com/",
+    'projectId': "fir-auth-ddfd6",
+    'storageBucket': "fir-auth-ddfd6.appspot.com",
+    'messagingSenderId': "247106769958",
+    'appId': "1:247106769958:web:9df424102f1e67d92442e7",
+    'measurementId': "G-87VT2PCEEY"}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
 
 class Login(QDialog):
     def __init__(self):
@@ -10,11 +22,17 @@ class Login(QDialog):
         loadUi(r'..\Resource\login.ui', self)
         self.loginbutton.clicked.connect(self.loginfunction)
         self.createaccbutton.clicked.connect(self.gotocreate)
+        self.loginInvalidMessage.setVisible(False)
+        self.loginMessage.setVisible(False)
 
     def loginfunction(self):
         email = self.email.text()
         password = self.password.text()
-        print("Successfully logged in with email: ", email, "and password:", password)
+        try:
+            auth.sign_in_with_email_and_password(email, password)
+            self.loginMessage.setVisible(True)
+        except:
+            self.loginInvalidMessage.setVisible(True)
 
     def gotocreate(self):
         createacc = CreateAcc()
@@ -26,15 +44,20 @@ class CreateAcc(QDialog):
         super(CreateAcc, self).__init__()
         loadUi(r'..\Resource\createacc.ui', self)
         self.signupbutton.clicked.connect(self.createaccfunction)
+        self.invalid.setVisible(False)
 
     def createaccfunction(self):
         email = self.email.text()
         if self.password.text() == self.confirmpass.text():
             password = self.password.text()
-            print("Successfully created acc with email: ", email, "and password: ", password)
-            login = Login()
-            widget.addWidget(login)
-            widget.setCurrentIndex(widget.currentIndex() + 1)
+            try:
+                auth.create_user_with_email_and_password(email, password)
+                login = Login()
+                widget.addWidget(login)
+                widget.setCurrentIndex(widget.currentIndex() + 1)
+            except:
+                self.invalid.setVisible(True)
+
 
 
 
