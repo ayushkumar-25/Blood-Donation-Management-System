00fd1fdaa54bf42e4b4b3e838754dda9d24747bf
ayushkumar25 <ayush2608@gmail.com>
2021-01-16 22:08:29 +0530

    request blood

--- Activity/requestblood.py
+++ Activity/requestblood.py
@@ -13,8 +13,17 @@ class RequestBlood(QDialog):
         super(RequestBlood, self).__init__()
         loadUi(r'..\Resource\requestblood.ui', self)
         self.backButton.clicked.connect(self.backtoHome)
+        self.requestbutton.clicked.connect(self.bloodRequest)
 
     def backtoHome(self):
         home = h.Home()
         m.widget.addWidget(home)
         m.widget.setCurrentIndex(m.widget.currentIndex() + 1)
+
+    def bloodRequest(self):
+        bloodGroup = self.bloodGroup.currentText()
+        location = self.location.currentText()
+        users = m.db.child('Users').get()
+        for user in users.each():
+            if user.val()['Blood Group'] == bloodGroup and user.val()['Location'] == location:
+                print(user.val())
